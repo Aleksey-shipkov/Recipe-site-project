75f20f6436edbc8e85490b6608c7ef2b2b07f002
Anya S <shipkov.alexey@gmail.com>
2022-11-26 22:21:57 +0300

    Donev11

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters import rest_framework as filters
-from food.models import Recipes
+from food.models import Recipes, Tag
 
 
 class RecipesFilter(filters.FilterSet):
@@ -7,10 +7,14 @@ class RecipesFilter(filters.FilterSet):
         field_name='author__id',
         lookup_expr='contains'
     )
-    tags = filters.CharFilter(
-        field_name='tags__slug',
-        lookup_expr='contains'
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug', to_field_name='slug',
+        queryset=Tag.objects.all()
     )
+    #tags = filters.CharFilter(
+        #field_name='tags__slug',
+        #lookup_expr='contains'
+    #)
 
     class Meta:
         model = Recipes
